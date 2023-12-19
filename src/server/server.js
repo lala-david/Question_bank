c331aa15b941c4f60edea6bf4df96e550df2f5b1
lala-david <wnswns1946@naver.com>
2023-12-19 23:03:55 +0900

    # 🐸 net

--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 const port = 8800;
 
-const logDirectory = path.join(__dirname, 'logs');
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory); 
-
-function getLogFileName() {
-    const date = new Date();
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.log`;
-}
-
-app.use((req, res, next) => {
-    const logFileName = getLogFileName();
-    const logFilePath = path.join(logDirectory, logFileName);
-    const logStream = fs.createWriteStream(logFilePath, { flags: 'a' }); 
-    const logMessage = `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`;
-
-    console.log(logMessage);
-    logStream.write(logMessage + '\n');
-    logStream.end();
-
+app.use(morgan('dev'));
+app.use((req, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
     next();
 });
+
 
 app.use(express.static(path.join(__dirname, '..', '..')));
 
